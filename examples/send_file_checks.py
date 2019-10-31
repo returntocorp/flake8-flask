3114b10b0c772aa58ef89d3f97c0b81e7f00b85b
grayson <grayson@returntocorp.com>
2019-10-30 17:31:58 -0700

    Changes examples to be more sensible and readable

--- examples/send_file_checks.py
+++ examples/send_file_checks.py
@@ -1,47 +1,60 @@
 ## SHOULD ALERT
+
 # flask.send_file case
-import flask
-flask.send_file(open("file.txt", 'r'))
+def example1():
+    import flask
+    flask.send_file(open("file.txt", 'r'))
 
 # With keyword args
-import flask
-flask.send_file(open("file.txt", 'r'), conditional=False)
+def example2():
+    import flask
+    flask.send_file(open("file.txt", 'r'), conditional=False)
 
 # from flask import send_file case
-from flask import send_file
-send_file(open("file.txt", 'r'))
+def example3():
+    from flask import send_file
+    send_file(open("file.txt", 'r'))
 
 # Variable resolution
-from flask import send_file
-fin = open("file.txt", 'r')
-send_file(fin)
+def example4():
+    from flask import send_file
+    fin = open("file.txt", 'r')
+    send_file(fin)
 
 # Variable resolution with other statements
-from flask import send_file
-fin = open("file.txt", 'r')
-print("random statement")
-send_file(fin, conditional=False)
+def example5():
+    from flask import send_file
+    fin = open("file.txt", 'r')
+    print("random statement")
+    send_file(fin, conditional=False)
 
 ## SHOULD NOT ALERT
-# Has a mime_type
-import flask
-flask.send_file(open("file.txt", 'r'), mime_type="text/plain")
 
-import flask
-flask.send_file(open("file.txt", 'r'), mime_type="text/plain", conditional=False)
+# Has a mimetype
+def example6():
+    import flask
+    flask.send_file(open("file.txt", 'r'), mimetype="text/plain")
+
+def example7():
+    import flask
+    flask.send_file(open("file.txt", 'r'), mimetype="text/plain", conditional=False)
 
 # Has a attachment_filename
-from flask import send_file
-fin = open("file.txt", 'r')
-send_file(fin, as_attachment=True, attachment_filename="file.txt")
+def example8():
+    from flask import send_file
+    fin = open("file.txt", 'r')
+    send_file(fin, as_attachment=True, attachment_filename="file.txt")
 
-from flask import send_file
-fin = open("file.txt", 'r')
-send_file(fin, as_attachment=True, attachment_filename="file.txt", conditional=False)
+def example9():
+    from flask import send_file
+    fin = open("file.txt", 'r')
+    send_file(fin, as_attachment=True, attachment_filename="file.txt", conditional=False)
 
 # String argument for arg0
-import flask
-flask.send_file("/tmp/file.txt")
+def example10():
+    import flask
+    flask.send_file("/tmp/file.txt")
 
-import flask
-flask.send_file(os.path.join("data", "file.txt"))
+def example11():
+    import flask, os
+    flask.send_file(os.path.join("data", "file.txt"))
