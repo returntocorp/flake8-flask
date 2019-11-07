fa1f056cc4e4a2d6dff7f9dd7f15ff0bbe1f0eb4
grayson <grayson@returntocorp.com>
2019-11-07 12:50:06 -0800

    Added SecureSetCookies to setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ import setuptools
 requires = ["flake8 > 3.6.0"]
 
 setuptools.setup(
-    name='flake8_flask',
-    version='0.0.7',
-    description='r2c checks for flask',
-    author='grayson',
-    author_email='grayson@r2c.dev',
+    name="flake8_flask",
+    version="0.0.8",
+    description="r2c checks for flask",
+    author="grayson",
+    author_email="grayson@r2c.dev",
     url="https://github.com/returntocorp/flake8-flask",
-    package_dir={'': 'src/'},
-    packages=['flake8_flask'],
+    package_dir={"": "src/"},
+    packages=["flake8_flask"],
     entry_points={
-        'flake8.extension': [
-            'R2C202=flake8_flask.send_file_checks:SendFileChecks',
+        "flake8.extension": [
+            "R2C202=flake8_flask.send_file_checks:SendFileChecks",
+            "R2C203=flake8_flask.secure_set_cookies:SecureSetCookies",
         ],
     },
     classifiers=[
@@ -22,5 +23,5 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-    ]
+    ],
 )
