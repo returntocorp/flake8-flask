51c7f5f51fea80255efbee80d48b65f84afd5777
grayson <grayson@returntocorp.com>
2019-11-08 14:47:21 -0800

    Added long description to setup.py; Version -> 0.1.1

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ import setuptools
 
 requires = ["flake8 > 3.6.0"]
 
+with open("README.md") as fin:
+    long_description = fin.read()
+
 setuptools.setup(
     name="flake8_flask",
-    version="0.1.0",
-    description="r2c checks for flask",
+    version="0.1.1",
+    description="flake8 plugin with checks for the Flask framework, by r2c",
+    long_description=long_description,
     author="grayson",
     author_email="grayson@r2c.dev",
-    url="https://github.com/returntocorp/flake8-flask",
+    url="https://r2c.dev",
     package_dir={"": "src/"},
     packages=["flake8_flask"],
+    python_requires=">=3.6",
     entry_points={
         "flake8.extension": [
             "R2C202=flake8_flask.send_file_checks:SendFileChecks",
@@ -23,5 +28,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Quality Assurance"
     ],
 )
